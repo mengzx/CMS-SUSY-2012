d1bf0cbdbab671a11bdf44686fe75107bffa8d32
Zhaoxia Meng <Zhaoxia.Meng@cern.ch>
2012-09-17 20:18:30 +0200

    NBjets

--- playHists/NBjets.C
+++ playHists/NBjets.C
@@ -1,18 +1,42 @@
 #include "NBjets.h"
 
+#include "playHist2D.h"
+#include "menus.h"
+#include "vectors.h"
+
+#include <math.h>
+#include <vector>
 using namespace std;
 
 
 NBjets::NBjets(){}
 
-double NBjets::Ebsion( vector<TFile*> invf, vector<TString> vdirname, int inibin ){
+vector<double> NBjets::getTrigEff( int whichpart, TString HTBins, TString MuonNumber ){
+  vector<double> trigeff;
+  if( doTrigCorr_ ){
+    if( whichpart == 1 ){
+      trigeff=HTATTrigEff_pushback(HTBins);
+    } else {
+      if( MuonNumber == "OneMuon_"){
+	trigeff=SingleMuTrigEff_pushback(HTBins);
+      } else if( MuonNumber == "DiMuon_"){
+	trigeff=DiMuTrigEff_pushback(HTBins);
+      }
+    }
+  } else {
+    trigeff=nominaltrigeff_pushback(HTBins);
+  }
+  return trigeff;
+}
+
+double NBjets::Ebsion( vector<TFile*> invf, vector<TString> vdirname, int inibin, vector<double> trigeff ){
   vector<TString> vhname_num;
-  vectocr<TString> vhname_dom;
+  vector<TString> vhname_dom;
   vhname_num.push_back("nJetMatchBPartonAndTagged_CommJetgeq2_h_all");
   vhname_dom.push_back("nJetMatchBParton_CommJetgeq2_h_all");
   playHist2D hf2d=playHist2D();
-  TH1D *numh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_num, trigeff );
-  TH1D *domh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_dom, trigeff );
+  TH2D *numh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_num, trigeff );
+  TH2D *domh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_dom, trigeff );
 
   double num=numh->Integral(1, 20, inibin, 10);
   double dom=domh->Integral(1, 10, inibin, 10);
@@ -23,14 +47,14 @@ double NBjets::Ebsion( vector<TFile*> invf, vector<TString> vdirname, int inibin
 
 }
 
-double NBjets::Im( vector<TFile*> invf, vector<TString> vdirname, int inibin ){
+double NBjets::Im( vector<TFile*> invf, vector<TString> vdirname, int inibin, vector<double> trigeff ){
   vector<TString> vhname_num;
-  vectocr<TString> vhname_dom;
+  vector<TString> vhname_dom;
   vhname_num.push_back("nJetMatchNonBPartonAndTagged_CommJetgeq2_h_all");
   vhname_dom.push_back("nJetMatchNonBParton_CommJetgeq2_h_all");
   playHist2D hf2d=playHist2D();
-  TH1D *numh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_num, trigeff );
-  TH1D *domh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_dom, trigeff );
+  TH2D *numh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_num, trigeff );
+  TH2D *domh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_dom, trigeff );
 
   double num=numh->Integral(1, 20, inibin, 10);
   double dom=domh->Integral(1, 10, inibin, 10);
@@ -41,36 +65,42 @@ double NBjets::Im( vector<TFile*> invf, vector<TString> vdirname, int inibin ){
 }
 
 
-double NBjets::N_bs( vector<TFile*> invf, vector<TString> vdirname, int inibin ){
+double NBjets::N_bs( vector<TFile*> invf, vector<TString> vdirname, int inibin, vector<double> trigeff ){
 
   vector<TString> vhname_num;
-  vhname_num.push_back("njet_CommJetgeq2_h_all");
+  vector<TString> vhname_dom;
+  vhname_num.push_back("nJetMatchBParton_CommJetgeq2_h_all");
+  vhname_dom.push_back("nJetMatchNonBParton_CommJetgeq2_h_all");
   playHist2D hf2d=playHist2D();
-  TH1D *numh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_num, trigeff );
+  TH2D *numh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_num, trigeff );
+  TH2D *domh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_dom, trigeff );
 
   double num=numh->Integral(1, 20, inibin, 10);
+  double dom=domh->Integral(1, 10, inibin, 10);
 
-  return num;
+  double im=num+dom;
+
+  return im;
 }
 
-double NBjets::N_bmatch( vector<TFile*> invf, vector<TString> vdirname, int inibin ){
+double NBjets::N_bmatch( vector<TFile*> invf, vector<TString> vdirname, int inibin, vector<double> trigeff ){
 
   vector<TString> vhname_num;
   vhname_num.push_back("nJetMatchBParton_CommJetgeq2_h_all");
   playHist2D hf2d=playHist2D();
-  TH1D *numh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_num, trigeff );
+  TH2D *numh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_num, trigeff );
 
   double num=numh->Integral(1, 20, inibin, 10);
 
   return num;
 }
 
-double NBjets::N_nonbmatch( vector<TFile*> invf, vector<TString> vdirname, int inibin ){
+double NBjets::N_nonbmatch( vector<TFile*> invf, vector<TString> vdirname, int inibin, vector<double> trigeff ){
 
   vector<TString> vhname_num;
   vhname_num.push_back("nJetMatchNonBParton_CommJetgeq2_h_all");
   playHist2D hf2d=playHist2D();
-  TH1D *numh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_num, trigeff );
+  TH2D *numh=hf2d.addHistForDiffFoldersFilesHists2D(invf, vdirname, vhname_num, trigeff );
 
   double num=numh->Integral(1, 20, inibin, 10);
 
@@ -78,14 +108,235 @@ double NBjets::N_nonbmatch( vector<TFile*> invf, vector<TString> vdirname, int i
 }
 
 
-double NBjets::nb0( vector<TFile*> invf, vector<TString> vdirname, int inibin ){
-
-  double ebsion=Ebsion( invf, vdirname, inibin );
-  double im=Im( invf, vdirname, inibin );
-  double n_bs=N_bs( invf, vdirname, inibin );
-  double n_bmatch=N_bmatch( invf, vdirname, inibin );
-  double n_nonbmatch=N_nonbmatch( invf, vdirname, inibin );
-
-nb0=
+TString NBjets::getDir( int whichpart, bool MuAddOrNot){
+  TString dir;
+  if( whichpart == 1 ){
+    dir = inidir_ + "rootfiles/hadronicSele" + subdir_;
+  } else if ( whichpart != 1 && MuAddOrNot == true && normalEstimation_ == false){
+    dir = inidir_ + "rootfiles/oneMuonSele/muonpT50GeV" + subdir_;
+  } else if ( whichpart !=1 && MuAddOrNot == false && normalEstimation_ == false){
+    dir = inidir_ + "rootfiles/oneMuonSele/muonpT10GeV" + subdir_;
+  } else if ( whichpart !=1 && normalEstimation_ == true){
+    dir = inidir_ + "rootfiles/oneMuonSele/muonpT45GeV" + subdir_;
+  }
+  return dir;
 }
+
+vector<TFile*> NBjets::getMCvf( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, bool MuAddOrNot ){
+  TString dir=getDir( whichpart, false );
+  vector<TFile*> MCvf;
+  if( whichpart == 1 ){
+    MCvf=MCvf_pushback(dir, MCsample_, "HadSele"+signalTrig_, HTBins, separateSample, singleMCsample );
+  } else if( whichpart != 1 && MuAddOrNot == true && normalEstimation_ == false ){
+    MCvf=MCvf_pushback(dir, MCsample_, "MuonAdded"+HadTaucontrolTrig_, HTBins, separateSample, singleMCsample );
+  } else if ( whichpart != 1 && MuAddOrNot == false && normalEstimation_ == false ){
+    MCvf=MCvf_pushback(dir, MCsample_, "Muon"+NotHadTaucontrolTrig_, HTBins, separateSample, singleMCsample );
+  } else if ( whichpart != 1 && normalEstimation_ == true ) {
+    MCvf=MCvf_pushback(dir, MCsample_, "Muon"+NormalcontrolTrig_, HTBins, separateSample, singleMCsample );
+  }
+
+  return MCvf;
+}
+
+vector<TString> NBjets::getVdirname( int whichpart, TString HTBins, TString MuonNumber, bool MuAddOrNot ){
+  vector<TString> vdirname;
+  if( whichpart == 1 ){
+    vdirname=dirName_pushback(folderlabel_+"", HTBins);
+  } else if( whichpart != 1 && MuAddOrNot == true && normalEstimation_ == false ){
+    vdirname=dirName_pushback(folderlabel_ + MuonNumber, HTBins);
+  } else if ( whichpart != 1 && MuAddOrNot == false && normalEstimation_ == false ){
+    vdirname=dirName_pushback(folderlabel_ + MuonNumber, HTBins);
+  } else if ( whichpart != 1 && normalEstimation_ == true ) {
+    vdirname=dirName_pushback(folderlabel_ + MuonNumber, HTBins);
+  }
+
+  return vdirname;
+}
+
+double NBjets::getEff( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> trigeff ){
+  vector<TFile*> mcvf=getMCvf( whichpart, HTBins, separateSample, singleMCsample, false );
+  vector<TString> vdir=getVdirname( whichpart, HTBins, MuonNumber, false );
+  int inibin=8;
+  if( whichpart != 1 ){
+    if( notCutAlphaT_ ){
+      inibin=1;
+    }
+  }
+
+  double eff=Ebsion( mcvf, vdir, inibin,trigeff );
+
+  closefV();
+  return eff;
+}
+
+double NBjets::getMistag( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString  MuonNumber, vector<double> trigeff ){
+  vector<TFile*> mcvf=getMCvf( whichpart, HTBins, separateSample, singleMCsample, false );
+  vector<TString> vdir=getVdirname( whichpart, HTBins, MuonNumber, false );
+  int inibin=8;
+  if( whichpart != 1 ){
+    if( notCutAlphaT_ ){
+      inibin=1;
+    }
+  }
+
+  double im=Im( mcvf, vdir, inibin,trigeff );
+
+  closefV();
+  return im;
+}
+
+double NBjets::getNbs( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> trigeff ){
+  vector<TFile*> mcvf=getMCvf( whichpart, HTBins, separateSample, singleMCsample, false );
+  vector<TString> vdir=getVdirname( whichpart, HTBins, MuonNumber, false );
+  int inibin=8;
+  if( whichpart != 1 ){
+    if( notCutAlphaT_ ){
+      inibin=1;
+    }
+  }
+
+  double im=N_bs( mcvf, vdir, inibin, trigeff );
+
+  closefV();
+  return im;
+}
+
+
+double NBjets::ZeroB( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> trigeff ){
+  double eff=getEff( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double mis=getMistag( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double nbs=getNbs( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double zerob=0;
+  for( int ib=0; ib<=4; ib++){
+    for( int is=0; is<=8; is++){
+      zerob = zerob + ( nbs * pow((1.-eff),ib) * pow((1.-mis),is) );
+    }
+  }
+
+  return zerob;
+}
+
+
+double NBjets::OneB( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> trigeff ){
+  double eff=getEff( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double mis=getMistag( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double nbs=getNbs( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double b1=0; double b2=0;
+  for( int ib=0; ib<=4; ib++){
+    for( int is=1; is<=8; is++){
+      b1 = b1 + ( nbs * pow((1.-eff),ib) * is * mis * pow((1.-mis),is-1) );
+    }
+  }
+
+  for( int ib=1; ib<=4; ib++){
+    for( int is=0; is<=8; is++){
+      b2 = b2 + ( nbs * ib * eff * pow((1.-eff),ib-1) * pow((1.-mis),is) );
+    }
+  }
+
+  return b1+b2;
+}
+
+
+double NBjets::TwoB( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> trigeff ){
+  double eff=getEff( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double mis=getMistag( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double nbs=getNbs( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double b1=0; double b2=0; double b3=0;
+  for( int ib=0; ib<=4; ib++){
+    for( int is=2; is<=8; is++){
+      b1 = b1 + nbs * pow((1.-eff),ib) * (is*(is-1)/2) * pow(mis,2) * pow((1.-mis),is-2);
+    }
+  }
+
+  for( int ib=2; ib<=4; ib++){
+    for( int is=0; is<=8; is++){
+      b2 = b2 + nbs * (ib*(ib-1)/2) * pow(eff,2) * pow((1.-eff),ib-2) * pow((1.-mis),is);
+    }
+  }
+
+  for( int ib=1; ib<=4; ib++){
+    for( int is=1; is<=8; is++){
+      b3 = b3 + nbs * ib * eff * pow((1.-eff),ib-1) * is * mis * pow((1.-mis),is-1);
+    }
+  }
+
+  return b1+b2+b3;
+}
+
+double NBjets::ThreeB( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> trigeff ){
+  double eff=getEff( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double mis=getMistag( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double nbs=getNbs( whichpart, HTBins, separateSample, singleMCsample, MuonNumber, trigeff );
+  double b1=0; double b2=0; double b3=0; double b4=0;
+  for( int ib=0; ib<=4; ib++){
+    for( int is=3; is<=8; is++){
+      b1 = b1 + nbs * pow((1.-eff),ib) * (is*(is-1)*(is-2)/6) * pow(mis,3) * pow((1.-mis),is-3);
+    }
+  }
+
+  for( int ib=3; ib<=4; ib++){
+    for( int is=0; is<=8; is++){
+      b2 = b2 + nbs * (ib*(ib-1)*(ib-2)/6) * pow(eff,3) * pow((1.-eff),ib-3) * pow((1.-mis),is);
+    }
+  }
+
+  for( int ib=1; ib<=4; ib++){
+    for( int is=2; is<=8; is++){
+      b3 = b3 + nbs * ib * eff * pow((1.-eff),ib-1) * (is*(is-1)/2) * pow(mis,2) * pow((1.-mis),is-2);
+    }
+  }
+
+  for( int ib=2; ib<=4; ib++){
+    for( int is=1; is<=8; is++){
+      b4 = b4 + nbs * (ib*(ib-1)/2) * pow(eff,2) * pow((1.-eff),ib-2) * is * mis * pow((1.-mis),is-1);
+    }
+  }
+
+  return b1+b2+b3+b4;
+}
+
+
+
+
+
+void NBjets::restults(){
+
+  /*  //Hadronic
+  int whichpart=1;
+  TString MuonNumber="";
+  vector<TString> vHTBins;
+  vHTBins.push_back("275");
+  vHTBins.push_back("325");
+  vHTBins.push_back("375");
+  vHTBins.push_back("475");
+  vHTBins.push_back("575");
+  vHTBins.push_back("675");
+  vHTBins.push_back("775");
+  vHTBins.push_back("875");
+  cout<<" Hadroni selection "<<endl;
+  for( unsigned int i=0; i<vHTBins.size(); i++ ){
+    double eff=getEff( whichpart, HTBins[i], false, "", "" );
+    double mis=getMistag( whichpart, HTBins[i], false, "", "" );
+    cout<<vHTBins[i]<<": "<<"Eff: "<<eff<<" MisTag: "<<mis<<endl;
+  }
+
+  whichpart=2;
+  MuonNumber="OneMuon_";
+  cout<<" one muon selection "<<endl;
+  for( unsigned int i=0; i<vHTBins.size(); i++ ){
+    double eff=getEff( whichpart, HTBins[i], false, "", MuonNumber );
+    double mis=getMistag( whichpart, HTBins[i], false, "", MuonNumber );
+    cout<<vHTBins[i]<<": "<<"Eff: "<<eff<<" MisTag: "<<mis<<endl;
+  }
+
+  MuonNumber="DiMuon_";
+  cout<<" Dimuon selection "<<endl;
+  for( unsigned int i=0; i<vHTBins.size(); i++ ){
+    double eff=getEff( whichpart, HTBins[i], false, "", MuonNumber );
+    double mis=getMistag( whichpart, HTBins[i], false, "", MuonNumber );
+    cout<<vHTBins[i]<<": "<<"Eff: "<<eff<<" MisTag: "<<mis<<endl;
+    }*/
+}
+
 
--- playHists/NBjets.h
+++ playHists/NBjets.h
@@ -12,14 +12,27 @@ class NBjets : public menus, public vectors{
   NBjets();
   ~NBjets(){;}
 
-  double Ebsion( vector<TFile*> invf, vector<TString> vdirname, int inibin );
-  double Im( vector<TFile*> invf, vector<TString> vdirname, int inibin );
-  double N_bs( vector<TFile*> invf, vector<TString> vdirname, int inibin );
-  double N_bmatch( vector<TFile*> invf, vector<TString> vdirname, int inibin );
-  double N_nonbmatch( vector<TFile*> invf, vector<TString> vdirname, int inibin );
+  vector<double>getTrigEff( int whichpart, TString HTBins, TString MuonNumber );
+  double Ebsion( vector<TFile*> invf, vector<TString> vdirname, int inibin, vector<double> triggeff );
+  double Im( vector<TFile*> invf, vector<TString> vdirname, int inibin, vector<double> triggeff );
+  double N_bs( vector<TFile*> invf, vector<TString> vdirname, int inibin, vector<double> triggeff );
+  double N_bmatch( vector<TFile*> invf, vector<TString> vdirname, int inibin, vector<double> triggeff );
+  double N_nonbmatch( vector<TFile*> invf, vector<TString> vdirname, int inibin, vector<double> triggeff );
 
-  double Nb0( vector<TFile*> invf, vector<TString> vdirname, int inibin );
+  TString getDir( int whichpart, bool MuAddOrNot );
+  vector<TFile*> getMCvf( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, bool MuAddOrNot );
+  vector<TString> getVdirname( int whichpart, TString HTBins, TString MuonNumber, bool MuAddOrNot );
+  double getEff( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> triggeff );
+  double getMistag( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> triggeff );
 
+  double getNbs( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> triggeff );
+  double ZeroB( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> triggeff );
+  double OneB( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> triggeff );
+  double TwoB( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> triggeff );
+  double ThreeB( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> triggeff );
+  double FourB( int whichpart, TString HTBins, bool separateSample, TString singleMCsample, TString MuonNumber, vector<double> triggeff );
+
+  void restults();
 };//NBjets
 
 #endif
